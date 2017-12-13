b8fcab9928503f7c1fd64e21182a5beaa1056981
Sysnando <sysnando@gmail.com>
2017-12-13 15:51:49 +0000

    Testing Jenkins pool

--- src/osgi/rest/example/Activator.java
+++ src/osgi/rest/example/Activator.java
@@ -14,6 +14,7 @@ public class Activator implements BundleActivator {
 	    registration = context.registerService( RestServiceExample.class.getName(), exampleService, null );
 	  }
 	  //testando o jenkins
+	  //testando o jenkins1
 	  @Override
 	  public void stop( BundleContext context ) throws Exception {
 		  
